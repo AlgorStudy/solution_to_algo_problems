5c49bf84d07cd5bd9994109914c4bdb983041a9e
hkw2028 <hkw2028@gmail.com>
2015-01-06 22:32:01 +0900

    complete Environment Configuration

---
+++ com.algo.practice/solution_to_algo_problems/TestCase.java
@@ -0,0 +1,10 @@
+package solution_to_algo_problems;
+
+class TestCase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print( "Environment Configuration is completed" );
+	}
+
+}
