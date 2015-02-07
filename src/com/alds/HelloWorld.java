1a7633bd90e67546a74614bf2136476d33a58e65
hkw2028 <hkw2028@gmail.com>
2015-02-07 11:01:19 +0900

    refactoring

---
+++ src/com/alds/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.alds;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		assert 1 == 2;
+		System.out.println("Hello World");
+	}
+
+}
