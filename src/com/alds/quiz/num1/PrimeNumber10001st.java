d216d5033a1df01ba2b45dedcb091abb947d95b8
hkw2028 <hkw2028@gmail.com>
2015-02-09 20:57:26 +0900

    arrange formatting

--- src/com/alds/quiz/num1/PrimeNumber10001st.java
+++ src/com/alds/quiz/num1/PrimeNumber10001st.java
@@ -11,8 +11,9 @@ public class PrimeNumber10001st {
 		for(int i = 0 ; i < len ; ){			
 			boolean isPrime = true;
 			for(int j = 0 ; j <= i// 지금까지 확인된 소수로 나누는데
-					&& primeNumbers[j] <= (long)Math.sqrt(targetNumber) // 12의 예에서 보듯이 1,2,3,4,6,12 일 때 3으로 나누어 떨어진다는 의미는 4로도 나누어 떨어진다는 의미이므로
+					// 12의 예에서 보듯이 1,2,3,4,6,12 일 때 3으로 나누어 떨어진다는 의미는 4로도 나누어 떨어진다는 의미이므로
 					// root값이하 까지의 소수로만 나누어서 떨어지는지만 확인해도 무방하다.
+					&& primeNumbers[j] <= (long)Math.sqrt(targetNumber)					
 					; ++j){
 				isPrime &= (targetNumber % primeNumbers[j] !=0);
 				if(!isPrime){
